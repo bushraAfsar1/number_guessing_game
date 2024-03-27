c78778b548b3f4bc76b89c76484355a1c418afcf
bushraAfsar1 <126601448+bushraAfsar1@users.noreply.github.com>
2024-03-28 00:55:22 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let generatedNumber = Math.floor(Math.random() * 10);
+//  console.log(generatedNumber);
+const answer = await inquirer.prompt([
+    {
+     name: 'guessedNumber' , message : 'Enter your guessing number',  type: 'number'
+    }
+]);
+if (generatedNumber === answer.guessedNumber){
+    console.log(`you guessed right number ${answer.guessedNumber} is correct!`);
+}
+else if (generatedNumber > answer.guessedNumber){
+    console.log(`your guessed number ${answer.guessedNumber} is less than to the right number`);
+}
+else{
+    console.log(`your guessed number ${answer.guessedNumber} is greater than to the right number`);
+};
